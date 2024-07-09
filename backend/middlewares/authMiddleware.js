4290258f738c8c657eeffd3eeb4fc7635ae000b0
wpocoyo87 <khadijah.lkc@gmail.com>
2024-07-09 11:18:06 +0800

    sync the userModel.js to UserModel.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import User from "../models/userModel.js";
+import User from "../models/UserModel.js";
 
 // Middleware to protect routes
 export const protect = asyncHandler(async (req, res, next) => {

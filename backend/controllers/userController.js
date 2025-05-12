510f0091b4f4df8e1e087a3c718fed94ed7b4f8b
Khadijah Kasiran <khadijah.lkc@gmail.com>
2025-05-13 00:03:54 +0800

    update userModel.js import small letter

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import {
   loginUserService,
   getUserByEmailService,
 } from "../services/userService.js";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 
 // Controller to register a new user
 const registerUser = asyncHandler(async (req, res) => {

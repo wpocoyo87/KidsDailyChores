2f18210b80d5bed4052dacefe7afeb55c7cad99e
Khadijah Kasiran <khadijah.lkc@gmail.com>
2025-05-12 19:05:42 +0800

    fix: import config in dbConfig.js to enable MongoDB connection

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { config } from "./config.js";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {});
+    const conn = await mongoose.connect(config.MONGO_URI, {});
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {

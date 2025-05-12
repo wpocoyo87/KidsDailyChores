7b15dfde9368d7d91da31b04683e8d658be6242b
Khadijah Kasiran <khadijah.lkc@gmail.com>
2025-05-12 17:28:12 +0800

    Fix: Add getServerSideProps to insertTask page to prevent static export and SSR localStorage error

--- frontend/pages/insertTask.js
+++ frontend/pages/insertTask.js
@@ -1,3 +1,7 @@
 import dynamic from 'next/dynamic';
 const InsertTask = dynamic(() => import('../components/InsertTask'), { ssr: false });
-export default InsertTask; 
+export default InsertTask;
+
+export async function getServerSideProps() {
+  return { props: {} };
+} 
